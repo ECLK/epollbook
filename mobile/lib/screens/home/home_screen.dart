54c30f52ccbcfd2fc6a377a0e2dde025df54bb18
Sashika Nawarathne <hi@sashika.me>
2020-07-31 01:27:24 +0530

    Update UI

--- mobile/lib/screens/home/home_screen.dart
+++ mobile/lib/screens/home/home_screen.dart
@@ -10,6 +10,7 @@ import 'package:mobile/screens/error/error_screen.dart';
 import 'package:mobile/screens/home/search/search_screen.dart';
 import 'package:mobile/screens/loading/loading_screen.dart';
 import 'package:logger/logger.dart';
+import 'package:mobile/styles/color_palette.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -17,7 +18,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String _title;
+  // String _title;
   final AppBloc _appBloc = AppBloc();
 
   String _currentDivision = "Select one";
@@ -26,28 +27,28 @@ class _HomeScreenState extends State<HomeScreen> {
   String _currentStation = "Select one";
   List<String> _stations;
 
-  static List<TabItem> _navItems = List.of([
-    new TabItem(Icons.insert_chart, "", Colors.blueAccent),
-    new TabItem(Icons.search, "", Colors.blueAccent),
-    new TabItem(Icons.person, "", Colors.blueAccent),
-  ]);
+  // static List<TabItem> _navItems = List.of([
+  //   new TabItem(Icons.insert_chart, "", Colors.blueAccent),
+  //   new TabItem(Icons.search, "", Colors.blueAccent),
+  //   new TabItem(Icons.person, "", Colors.blueAccent),
+  // ]);
 
-  CircularBottomNavigationController _navController =
-      new CircularBottomNavigationController(1);
+  // CircularBottomNavigationController _navController =
+  //     new CircularBottomNavigationController(1);
 
-  void _setTitle() {
-    switch (_navController.value) {
-      case 0:
-        _title = "Analytics";
-        break;
-      case 1:
-        _title = "Search";
-        break;
-      case 2:
-        _title = "Profile";
-        break;
-    }
-  }
+  // void _setTitle() {
+  //   switch (_navController.value) {
+  //     case 0:
+  //       _title = "Analytics";
+  //       break;
+  //     case 1:
+  //       _title = "Search";
+  //       break;
+  //     case 2:
+  //       _title = "Profile";
+  //       break;
+  //   }
+  // }
 
   @override
   void initState() {
@@ -59,6 +60,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
           child: BlocProvider(
         lazy: true,
@@ -110,98 +112,274 @@ class _HomeScreenState extends State<HomeScreen> {
             .toSet()
             .toList();
 
-    return Center(
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text("Division"),
-          DropdownButton<String>(
-            value: _currentDivision,
-            items: _divisions
-                .map((String division) => DropdownMenuItem<String>(
-                      value: division,
-                      child: Text(division),
-                    ))
-                .toList(),
-            onChanged: (value) {
-              setState(() {
-                _currentDivision = value;
-              });
-              Logger().i(_currentDivision);
-            },
+          SizedBox(height: 40),
+          Text(
+            "Tell us about your station",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 60),
+          Text(
+            "Polling Division",
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              DropdownButton<String>(
+                value: _currentDivision,
+                items: _divisions
+                    .map((String division) => DropdownMenuItem<String>(
+                          value: division,
+                          child: Text(division),
+                        ))
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _currentDivision = value;
+                  });
+                  Logger().i(_currentDivision);
+                },
+              ),
+            ],
+          ),
+          SizedBox(height: 40),
           _currentDivision != "Select one"
               ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Station"),
-                    DropdownButton<String>(
-                      value: _currentStation,
-                      items: _stations
-                          .map((String station) => DropdownMenuItem<String>(
-                                value: station,
-                                child: Text(station),
-                              ))
-                          .toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          _currentStation = value;
-                        });
-                        Logger().i(_currentStation);
-                      },
+                    Text(
+                      "Station",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        DropdownButton<String>(
+                          value: _currentStation,
+                          items: _stations
+                              .map((String station) => DropdownMenuItem<String>(
+                                    value: station,
+                                    child: Text(station),
+                                  ))
+                              .toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _currentStation = value;
+                            });
+                            Logger().i(_currentStation);
+                          },
+                        ),
+                      ],
                     ),
                   ],
                 )
               : Container(),
-          SizedBox(height: 40),
-          FlatButton(
-            child: Text("Proceed"),
-            onPressed: _currentDivision != "Select one" &&
-                    _currentStation != "Select one"
-                ? () =>
-                    _handleProceed(context, _currentDivision, _currentStation)
-                : null,
+          Expanded(
+            child: Container(),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              InkWell(
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: _currentDivision != "Select one" &&
+                            _currentStation != "Select one"
+                        ? ColorPalette.primary
+                        : ColorPalette.idle,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 12,
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Proceed",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                onTap: _currentDivision != "Select one" &&
+                        _currentStation != "Select one"
+                    ? () => _handleProceed(
+                        context, _currentDivision, _currentStation)
+                    : null,
+              ),
+            ],
           )
         ],
       ),
     );
   }
 
-  _buildMethodSelectScreen(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        FlatButton(
-          child: Text("Back"),
-          onPressed: () => _goBack(context),
+  Widget _buildMethodSelectScreen(BuildContext context) {
+    return Container(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 40),
+                  Text(
+                    "What will you be doing",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: <Widget>[
+                InkWell(
+                  child: Container(
+                    height: 200,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(24),
+                        bottomRight: Radius.circular(24),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 12,
+                        ),
+                      ],
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/queue.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  onTap: () => _handleMethodSelect(context, 0),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: <Widget>[
+                InkWell(
+                  child: Container(
+                    height: 40,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(24),
+                        bottomRight: Radius.circular(24),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 12,
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text("Marking Queue"),
+                    ),
+                  ),
+                  onTap: () => _handleMethodSelect(context, 0),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                InkWell(
+                  child: Container(
+                    height: 200,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        bottomLeft: Radius.circular(24),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 12,
+                        ),
+                      ],
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/vote.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  onTap: () => _handleMethodSelect(context, 1),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                InkWell(
+                  child: Container(
+                    height: 40,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        bottomLeft: Radius.circular(24),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 12,
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text("Marking Vote"),
+                    ),
+                  ),
+                  onTap: () => _handleMethodSelect(context, 1),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+          ],
         ),
-        FlatButton(
-          child: Text("Pending"),
-          onPressed: () => _handleMethodSelect(context, 0),
-        ),
-        FlatButton(
-          child: Text("Queue"),
-          onPressed: () => _handleMethodSelect(context, 1),
-        )
-      ],
+      ),
     );
   }
-}
-
-void _handleMethodSelect(BuildContext context, int method) {
-  BlocProvider.of<AppBloc>(context).add(FetchElectors(method));
 }
 
 SearchScreen _buildElectorsLoadedScreen(
         BuildContext context, List<Elector> electors, bool isPending) =>
     SearchScreen(context, electors, isPending);
 
+ErrorScreen _buildErrorScreen(String error) => ErrorScreen(error);
+
+void _handleMethodSelect(BuildContext context, int method) {
+  BlocProvider.of<AppBloc>(context).add(FetchElectors(method));
+}
+
 void _handleProceed(
     BuildContext context, String _currentDivision, String _currentStation) {
   BlocProvider.of<AppBloc>(context)
       .add(SaveMeta(ELECTION, DISTRICT, _currentDivision, _currentStation));
 }
-
-void _goBack(BuildContext context) {
-  BlocProvider.of<AppBloc>(context).add(ChangeMeta(ELECTION));
-}
-
-ErrorScreen _buildErrorScreen(String error) => ErrorScreen(error);
--- mobile/lib/screens/home/search/search_screen.dart
+++ mobile/lib/screens/home/search/search_screen.dart
@@ -33,14 +33,34 @@ class _SearchScreenState extends State<SearchScreen> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              FlatButton(
-                child: Text("Back"),
-                onPressed: () => _goBack(widget.baseContext),
+              InkWell(
+                child: Icon(Icons.refresh),
+                onTap: () => _refresh(widget.baseContext, widget.isPending),
               ),
-              FlatButton(
-                child: Text("Refresh"),
-                onPressed: () => _refresh(widget.baseContext, widget.isPending),
-              ),
+              PopupMenuButton(
+                elevation: 2,
+                onSelected: (int value) {
+                  switch (value) {
+                    case 0:
+                      _goBack(widget.baseContext);
+                      break;
+
+                    case 1:
+                      _gotoMetadataChange(context);
+                      break;
+                  }
+                },
+                itemBuilder: (BuildContext context) => [
+                  PopupMenuItem(
+                    value: 0,
+                    child: Text("Change Position"),
+                  ),
+                  PopupMenuItem(
+                    value: 1,
+                    child: Text("Change Metadata"),
+                  )
+                ].toList(),
+              )
             ],
           ),
           // AwesomeButton(
@@ -64,17 +84,49 @@ class _SearchScreenState extends State<SearchScreen> {
           SizedBox(
             height: 40.0,
           ),
-          TextField(
-            decoration: InputDecoration(
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 12,
+                ),
+              ],
+            ),
+            child: TextField(
+              style: TextStyle(color: Colors.black),
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                focusColor: ColorPalette.primary,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(24),
+                  borderSide: BorderSide(width: 0),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(24),
+                  borderSide: BorderSide(width: 0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(24),
+                  borderSide: BorderSide(width: 1, color: ColorPalette.primary),
+                ),
                 hintText: "Search by Elector ID / NIC",
-                suffixIcon: Icon(Icons.search)),
-            // TODO: set autofocus
-            // autofocus: true,
-            controller: _editingController,
-            onChanged: (value) {
-              _onSearch(value);
-            },
+                suffixIcon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+              ),
+              // TODO: set autofocus
+              // autofocus: true,
+              controller: _editingController,
+              onChanged: (value) {
+                _onSearch(value);
+              },
+            ),
           ),
+          SizedBox(height: 20),
           Expanded(child: _buildSearchResults()),
         ],
       ),
@@ -82,18 +134,51 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 
   Widget _buildSearchResults() {
-    return ListView(
-        children: widget.electors
-            .map(
-              (elector) => ListTile(
-                title: Text(elector.id),
-                subtitle: Text(elector.nic),
-                onTap: () {
-                  _handleTap(elector);
-                },
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 12,
+          ),
+        ],
+      ),
+      child: widget.electors.isNotEmpty
+          ? ListView(
+              children: widget.electors
+                  .map(
+                    (elector) => ListTile(
+                      title: Text(
+                        elector.id,
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      subtitle: Text(elector.nic),
+                      onTap: () {
+                        _handleTap(elector);
+                      },
+                    ),
+                  )
+                  .toList())
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.warning,
+                    color: ColorPalette.idle,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Nothing so far",
+                    style: TextStyle(color: ColorPalette.idle),
+                  )
+                ],
               ),
-            )
-            .toList());
+            ),
+    );
   }
 
   void _scanQR() {
@@ -176,10 +261,10 @@ class _SearchScreenState extends State<SearchScreen> {
                 ),
                 FlatButton(
                   child: Text(
-                    "Update to In Queue",
+                    widget.isPending ? "Add to Queue" : "Mark as Voted",
                     style: TextStyle(color: Colors.white),
                   ),
-                  color: Colors.blueAccent,
+                  color: ColorPalette.primary,
                   onPressed: () =>
                       _updateVoterState(elector.id, widget.isPending),
                 )
@@ -202,6 +287,10 @@ class _SearchScreenState extends State<SearchScreen> {
 
   void _goBack(BuildContext context) {
     BlocProvider.of<AppBloc>(context).add(ChangeMethod());
+  }
+
+  void _gotoMetadataChange(BuildContext context) {
+    BlocProvider.of<AppBloc>(context).add(ChangeMeta(ELECTION));
   }
 
   void _refresh(BuildContext context, bool isPending) {
